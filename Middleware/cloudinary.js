699ebd9b10cd587042b24eebc1e7e5ab24013556
zohaib-khalid770 <zohaib.ch.2208@gmail.com>
2025-04-06 22:28:58 +0500

    install packages

--- Middleware/cloudinary.js
+++ Middleware/cloudinary.js
@@ -5,8 +5,10 @@ dotenv.config();
 
 cloudinary.config({
 	cloud_name: "dv0rjcjfe",
-	api_key:"992843568554731",
-	api_secret:"dVPGiwBPwQOw3H892AqeCLjmLOk",
+	api_key: "992843568554731",
+	api_secret: "dVPGiwBPwQOw3H892AqeCLjmLOk",
 });
 
-export default cloudinary;
+// export default cloudinary;
+// export const cloudinary
+export default cloudinary
